0df24e55d24150d673335f12e34d118afc1e7f83
Vladislav <spanky@yandex.ru>
2021-01-04 17:56:33 +0300

    Edited the output, added boost from the local repository in cmake

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "lib.h"
-
 #include <iostream>
 
 int main (int, char **)
 {
-    std::cout << "Version: " << version() << std::endl;
+    std::cout << "build " << version() << "\nHello world!" << std::endl;
+
+    int konec;
+    std::cin >> konec;
 
     return 0;
 }
